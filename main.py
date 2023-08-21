3764ebf0b0da3561d57351a8a48f94663fbd8325
yohanCami <yosanchezm@unal.edu.co>
2023-08-20 19:41:08 -0500

    Creación clases, atributos y métodos
    Lo que dice el titulo

---
+++ main.py
@@ -0,0 +1,63 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        
+        if (color == "rojo") or (color == "verde") or (color == "amarillo") or (color == "negro") or (color == "blanco"):
+
+            self.color = color
+
+     
+class Auto:
+    
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos(self):
+
+        return len(self.asientos)
+
+    def verificarIntegridad(self):
+
+        if self.registro == self.motor.registro:
+
+            for i in self.asientos:
+
+                if i.registro != self.registro:
+
+                    return "Las piezas no son originales"
+                
+            return "Auto original"
+        
+        return "Las piezas no son originales"
+
+
+class Motor:
+    
+    def __init__(self, numeroCilindros, tipo, registro):
+
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+
+        self.registro = registro
+    
+    def asignarTipo(self, tipo):
+
+        if (tipo == "electrico") or (tipo == "gasolina"):
+
+            self.tipo = tipo
+    
+
